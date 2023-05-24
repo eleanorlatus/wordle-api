5eabc487452de60cdcc1cc6eabf3be7fba8d3e4d
Eleanor Latus <eleanor@latus.co.uk>
2023-05-24 18:30:21 +0100

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-//const cors = require('cors')
+const cors = require('cors')
 const PORT = 3001
 
-//app.use(cors())
+app.use(cors())
 
 const letters = [
       {"key": "a"},
